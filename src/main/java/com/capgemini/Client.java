bc70be91d1e221aabbade1bb2ce242d1502b984c
Ranganth <86073572+Ranganth@users.noreply.github.com>
2021-07-19 18:05:17 +0530

    first

---
+++ src/main/java/com/capgemini/Client.java
@@ -0,0 +1,16 @@
+package com.capgemini;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+
+@SpringBootApplication
+
+public class Client {
+	public static void main(String[] args) {
+		SpringApplication.run(Client.class, args);
+		
+	}
+
+}
